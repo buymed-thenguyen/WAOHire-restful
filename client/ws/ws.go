6a36730e0358e8140e3c21bd6d6caacf51ee5935
thenguyen <nguyenthe0202.1997@gmail.com>
2025-06-20 17:06:22 +0700

    check env for init grpc

--- client/ws/ws.go
+++ client/ws/ws.go
@@ -7,18 +7,30 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
 	"log"
+	"os"
 )
 
 var grpcConn pb.BroadcasterClient
 
 func InitGRPCClient(cfg *config.GrpcClient) {
-	creds := credentials.NewClientTLSFromCert(nil, "")
+	var opts []grpc.DialOption
+
+	if os.Getenv("ENV") == "prd" {
+		creds := credentials.NewClientTLSFromCert(nil, "")
+		opts = append(opts, grpc.WithTransportCredentials(creds))
+		log.Println("gRPC client using TLS (production mode)")
+	} else {
+		opts = append(opts, grpc.WithInsecure())
+		log.Println("gRPC client using insecure connection (non-production mode)")
+	}
+
 	conn, err := grpc.Dial(
 		fmt.Sprintf("%s:%s", cfg.Host, cfg.Port),
-		grpc.WithTransportCredentials(creds),
+		opts...,
 	)
 	if err != nil {
 		log.Fatalf("Failed to connect to gRPC: %v", err)
 	}
+
 	grpcConn = pb.NewBroadcasterClient(conn)
 }
