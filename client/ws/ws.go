b05c9ed6f83797f9172569c78da944310fd6d2e1
thenguyen <nguyenthe0202.1997@gmail.com>
2025-06-20 14:05:42 +0700

    dial with nil credential

--- client/ws/ws.go
+++ client/ws/ws.go
@@ -5,13 +5,18 @@ import (
 	pb "backend-ws/proto"
 	"fmt"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 	"log"
 )
 
 var grpcConn pb.BroadcasterClient
 
 func InitGRPCClient(cfg *config.Websocket) {
-	conn, err := grpc.Dial(fmt.Sprintf("%s:%s", cfg.Host, cfg.Port), grpc.WithInsecure())
+	creds := credentials.NewClientTLSFromCert(nil, "")
+	conn, err := grpc.Dial(
+		fmt.Sprintf("%s:%s", cfg.Host, cfg.Port),
+		grpc.WithTransportCredentials(creds),
+	)
 	if err != nil {
 		log.Fatalf("Failed to connect to gRPC: %v", err)
 	}
